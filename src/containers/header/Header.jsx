366420b02237aece0c34807ea6301e5ac8cd08f0
CHET <chestercatapia@yahoo.com>
2024-03-02 18:35:44 +0800

    Fix Header P

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => (
     </div>
     <h1 className="Chester">Hello, Chester Web Developer</h1>
       <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <p>Discover the limitless possibilities of GPT-3 through our platform. Whether you're a seasoned developer or new to AI technology, we provide the resources and support for you to create something extraordinary. Join our community and embark on a journey of innovation and creativity. Let's build something amazing together with GPT-3 from OpenAI.</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
